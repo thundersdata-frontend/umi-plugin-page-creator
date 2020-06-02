9ce83e36fe118c5e395943a3cb19b7b388326f7c
chj_damon <chjdamon@gmail.com>
2020-06-02 16:15:15 +0800

    feat: 完成对所有图表和组件的支持

--- src/screen/templates/generateCol.ts
+++ src/screen/templates/generateCol.ts
@@ -4,15 +4,46 @@ import { ScreenConfigPayloadCol, ColType, ChartConfig } from "../../../interface
 import { removeUnusedImport } from "../../utils/removeUnusedImport";
 
 export default function(colPath: string, col: ScreenConfigPayloadCol) {
-  console.log(col);
   const code = `
     import React from 'react';
     import { Col } from 'antd';
-    import { ChartDom, createColumnPlot } from '@td-design/charts';
+    import {
+      ChartDom,
+      ComBlock,
+      ComCard,
+      IconsScoreChart,
+      CustomTable,
+      createColumnPlot,
+      createColumnLinePlot,
+      createDonutPlot,
+      createRadialStackPlot,
+      createCustomBarPlot,
+      createGroupColumnPlot,
+      createGroupedColumnLinePlot,
+      createCustomGroupedBarPlot,
+      createLinePlot,
+      createRadarPlot,
+      createRangeColumnPlot,
+      createCustomRangeBarPlot,
+      createDonutRosePlot,
+      createScatterPlot,
+      createStackAreaPlot,
+      createStackColumnPlot,
+      createStackRosePlot,
+      createWaterfallPlot,
+      createLiquidPlot,
+    } from '@td-design/charts';
 
     export default () => {
       return (
-        <Col xs={${col.xs}} sm={${col.sm}} md={${col.md}} lg={${col.lg}} xl={${col.xl}} xxl={${col.xxl}}>
+        <Col
+          xs={${JSON.stringify(col.xs)}}
+          sm={${JSON.stringify(col.sm)}}
+          md={${JSON.stringify(col.md)}}
+          lg={${JSON.stringify(col.lg)}}
+          xl={${JSON.stringify(col.xl)}}
+          xxl={${JSON.stringify(col.xxl)}}
+        >
           <div style={{
             height: '100%',
             flex: 1,
@@ -20,9 +51,7 @@ export default function(colPath: string, col: ScreenConfigPayloadCol) {
             justifyContent: 'center',
             alignItems: 'center',
           }}>
-            <ChartDom
-              title="测试标题7"
-            />
+            ${renderComponent(col.type, col.chartConfig)}
           </div>
         </Col>
       );
@@ -63,76 +92,221 @@ function renderComponent(type: ColType, chartConfig: ChartConfig) {
         return createColumnPlot({
           dom,
           data: [],
-          config: ${JSON.stringify(restProps)}
+          config: ${JSON.stringify(restProps)},
         });
       `;
       break;
 
     case 'barLine':
+      code = `
+        return createColumnLinePlot({
+          dom,
+          data: [],
+          config: ${JSON.stringify(restProps)},
+        });
+      `;
       break;
 
     case 'circle':
+      code = `
+        return createDonutPlot({
+          dom,
+          data: [],
+          config: ${JSON.stringify(restProps)},
+        });
+      `;
       break;
 
     case 'circleStackBar':
+      code = `
+        return createRadialStackPlot({
+          dom,
+          data: [],
+          config: ${JSON.stringify(restProps)},
+        });
+      `;
       break;
 
     case 'column':
+      code = `
+        return createCustomBarPlot({
+          dom,
+          data: [],
+          config: ${JSON.stringify(restProps)},
+        });
+      `;
       break;
 
     case 'groupBar':
+      code = `
+        return createGroupColumnPlot({
+          dom,
+          data: [],
+          config: ${JSON.stringify(restProps)},
+        });
+      `;
       break;
 
     case 'groupBarLine':
+      code = `
+        return createGroupedColumnLinePlot({
+          dom,
+          data: [],
+          config: ${JSON.stringify(restProps)},
+        });
+      `;
       break;
 
     case 'groupColumn':
+      code = `
+        return createCustomGroupedBarPlot({
+          dom,
+          data: [],
+          config: ${JSON.stringify(restProps)},
+        });
+      `;
       break;
 
     case 'line':
+      code = `
+        return createLinePlot({
+          dom,
+          data: [],
+          config: ${JSON.stringify(restProps)},
+        });
+      `;
       break;
 
     case 'map':
+      code = `
+        return ;
+      `;
       break;
 
     case 'radar':
+      code = `
+        return createRadarPlot({
+          dom,
+          data: [],
+          config: ${JSON.stringify(restProps)},
+        });
+      `;
       break;
 
     case 'rangeBar':
+      code = `
+        return createRangeColumnPlot({
+          dom,
+          data: [],
+          config: ${JSON.stringify(restProps)},
+        });
+      `;
       break;
 
     case 'rangeColumn':
+      code = `
+        return createCustomRangeBarPlot({
+          dom,
+          data: [],
+          config: ${JSON.stringify(restProps)},
+        });
+      `;
       break;
 
     case 'rank':
+      code = `
+        return (
+          <ComBlock>
+            <ComCard title="${title}>
+              <IconsScoreChart standards={[]} scores={[]} />
+            </ComCard>
+          </ComBlock>
+        );
+      `;
       break;
 
     case 'rose':
+      code = `
+        return createDonutRosePlot({
+          dom,
+          data: [],
+          config: ${JSON.stringify(restProps)},
+        });
+      `;
       break;
 
     case 'scatter':
+      code = `
+        return createScatterPlot({
+          dom,
+          data: [],
+          config: ${JSON.stringify(restProps)},
+        });
+      `;
       break;
 
     case 'stackArea':
+      code = `
+        return createStackAreaPlot({
+          dom,
+          data: [],
+          config: ${JSON.stringify(restProps)},
+        });
+      `;
       break;
 
     case 'stackBar':
+      code = `
+        return createStackColumnPlot({
+          dom,
+          data: [],
+          config: ${JSON.stringify(restProps)},
+        });
+      `;
       break;
 
     case 'stackRose':
+      code = `
+        return createStackRosePlot({
+          dom,
+          data: [],
+          config: ${JSON.stringify(restProps)},
+        });
+      `;
       break;
 
     case 'table':
+      code = `
+        return (
+          <ComBlock>
+            <ComCard title="${title}>
+              <CustomTable columns={[]} dataSource={[]} enabledScroll />
+            </ComCard>
+          </ComBlock>
+        );
+      `;
       break;
 
     case 'waterfall':
+      code = `
+        return createWaterfallPlot({
+          dom,
+          data: [],
+          config: ${JSON.stringify(restProps)},
+        });
+      `;
       break;
 
     case 'wave':
+      code = `
+        return createLiquidPlot({
+          dom,
+          data: 50,
+        });
+      `;
       break;
   }
-  console.log(code);
-  const result = `
+  return `
     <ChartDom
       title="${title}"
       getDom={(dom: HTMLElement) => {
@@ -140,5 +314,4 @@ function renderComponent(type: ColType, chartConfig: ChartConfig) {
       }}
     />
   `;
-  console.log(result);
 }
